fb157f833e9b1966e56a25c5fedd46e7f1f9f2d2
greattwhiteshark <greattwhiteshark@gmail.com>
2024-11-12 00:17:58 +0530

    second commit

--- src/main/java/day2/realworldexample/ElectricalAppliance.java
+++ src/main/java/day2/realworldexample/ElectricalAppliance.java
@@ -5,8 +5,9 @@ package day2.realworldexample;
  * <p>every electrical appliance can be turned on and turned off </p>
  * <br>
  * house hold appliances cannot consume more than 1000 watts for safety purpose
- * @author leekendra
+ * @author leekendra venna
  * @version 1.0
+ *
  * */
 public interface ElectricalAppliance {
     final static int MAX_POWER = 1000;
